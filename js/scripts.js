02aa02055e9c5ceac8587776b7ea9258b113fd05
Nelson Urbina <urbinans@gmail.com>
2018-02-02 16:01:44 -0400

    Fixed all the return links

--- js/scripts.js
+++ js/scripts.js
@@ -36,7 +36,7 @@
     });
 
     // Scroll to top
-    $('#to-top').click(function() {
+    $('.to-top').click(function() {
         $('html, body').animate({
             scrollTop: 0
         }, 500);
